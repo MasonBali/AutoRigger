0c4f9982e0dfc8a6bc4ead025cf47135332d0e1a
Paul Schweizer <paulschweizer@gmx.net>
2013-09-24 22:22:27 +0200

    some data management tests

--- utility/datareader.py
+++ utility/datareader.py
@@ -9,24 +9,58 @@ reload(path)
 
 
 class DataReader():
+    """"""
     def __init__(self):
         pass
     # end def __init__
 
     def parse_xml(self, datatype, module):
+        """"""
+        rigmodule = RigModule() 
+        xml_result = dict()
         xml_path = os.path.join(path.Path().get_root_path(), 'data', datatype,
                                  ('%s.xml' % module))
         xmldoc = minidom.parse(xml_path)
         root = xmldoc.firstChild
-        for node in root.childNodes:
-            if node.nodeType == 1 and node.hasAttributes():
-                attributes = node.attributes
-                for key in attributes.keys():
-                    print attributes[key].value
-            
-            
+        
+        # moduletype
+        module = self.get_attributes(xmldoc.getElementsByTagName('rigmodule')[0], rigmodule)
+        self.set_attribute(rigmodule, 'moduletype', module['moduletype'])
+
+        #joints
+        for joint in xmldoc.getElementsByTagName('joint'):
+            self.set_attribute(rigmodule, 'joints', list())
+        
+        
+        
+        print dir(rigmodule)
     # end def parse_xml
-# end class DataReader()
+    
+    def get_attributes(self, node, rigmodule):
+        """"""
+        attributes = dict()
+        if node.nodeType == 1 and node.hasAttributes():
+            for key in node.attributes.keys():
+                attributes[key] = node.attributes[key].value
+            # end for key in node.attributes.keys()
+        return attributes
+    # end def get_attributes
+    
+    def set_attribute(self, rigmodule, attribute, value):
+        """"""
+        setattr(rigmodule, attribute, value)
+    # end def set_attribute
+# end class DataReader
+
+
+class RigModule():
+    def __init__(self):
+        self.module = 'arm'
+        self.joints = [] # list of joints
+
+
+
+
 
 dr = DataReader()
 dr.parse_xml('modules', 'arm')
