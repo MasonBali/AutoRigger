fdc016e8912676e47314f1ef0181cbd16b2fcc36
Paul Schweizer <schweipa@trixter.de>
2013-10-01 12:42:27 +0200

    minor cleanup in ui

--- ui/autoriggerui.py
+++ ui/autoriggerui.py
@@ -7,6 +7,8 @@ Created on 17.09.2013
 
 import os
 import sys
+from PySide import QtGui, QtCore
+from maya import cmds
 sys.path.append(os.path.dirname(__file__))
 from custompyside import *
 reload(dbutton)
@@ -30,11 +32,11 @@ class AutoRiggerUI(base_class, form_class):
         self.setupUi(self)
         self.setup_variables()
         self.setup_widgets()
-    # end def __init__
+    # end def __init__()
 
     def setup_variables(self):
         """#################################################################"""
-    # end def setup_variables
+    # end def setup_variables()
 
     def setup_widgets(self):
         """#################################################################"""
@@ -43,13 +45,13 @@ class AutoRiggerUI(base_class, form_class):
             btn = dbutton.DraggableButton()
             btn.setText(module)
             self.btnlay.insertWidget(0, btn)
-        # end for module in self.get_rigging_modules
+        # end for module in self.get_rigging_modules()
 
         #--- 1. A backdrop where the connections between the nodes get drawn
         self.backdrop = nodebackdrop.NodeBackDrop()
         self.backdrop.setLayout(dlayout.DragSupportLayout())
         self.draglay.addWidget(self.backdrop)
-
+        
         #--- 2. Create some test nodes
         main_node = node.Node()
         mid_node = node.Node()
@@ -58,14 +60,15 @@ class AutoRiggerUI(base_class, form_class):
         main_node.add_output(mid_node)
         main_node.add_output(end_node)
         mid_node.add_input(main_node)
-        mid_node.add_output(end_node)
-        end_node.add_input(mid_node)
-        end_node.add_input(main_node)
+        mid_node.add_output(end_node) 
+        end_node.add_input(mid_node)  
+        end_node.add_input(main_node)     
         for i, n in enumerate(nodes):
             self.backdrop.layout().addWidget(n)
             n.layout().addWidget(QtGui.QPushButton('%s' % i))
-        # end for i, n in enumerate(nodes)()
-    # end def setup_widgets
+        # end for i, n in enumerate(nodes)
+
+    # end def setup_widgets()
 
     def get_rigging_modules(self):
         """Retrieves a list of all available rigging modules."""
@@ -90,3 +93,5 @@ def main():
     autorigger_win = AutoRiggerUI()
     autorigger_win.show()
 # end def main
+
+main()
